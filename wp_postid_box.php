a2804f08d7c4b81bb81b5babc7168744c432dd3a
Chris Brosnan <cbrosnan00@gmail.com>
2017-10-11 12:53:29 +0100

    Minified / second version

--- wp_postid_box.php
+++ wp_postid_box.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 Plugin Name: WP Post ID Box
 Author: Chris Brosnan
@@ -11,20 +10,14 @@ function add_custom_meta_box()
     add_meta_box("demo-meta-box", "Post ID", "custom_meta_box_markup", "post", "side", "high", null);
 }
 add_action("add_meta_boxes", "add_custom_meta_box");
-
-
 /*Post ID Meta field*/
 function custom_meta_box_markup($object)
 {
     wp_nonce_field(basename(__FILE__), "meta-box-nonce");
-
     global $post;
     $id = $post->ID;
-
-    ?>
-        <div>
-            <!--- <label for="meta-box-text">Post ID</label> --->
-            <input name="meta-box-text" type="text" value="<?php echo $id; ?>" style="width: 100%;" readonly />
-        </div>
-    <?php
-} ?>
+        echo '<div>
+          	<label for="meta-box-text">Post ID</label>
+            <input name="meta-box-text" type="text" value="' . $id . '" style="width: 100%;" readonly />
+        </div>';
+}
