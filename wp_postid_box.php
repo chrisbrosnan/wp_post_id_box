37c0147058f50110a727a1f3a1d7293a94205aaf
Chris Brosnan <cbrosnan00@gmail.com>
2017-11-10 19:39:16 +0000

    Upload

--- wp_postid_box.php
+++ wp_postid_box.php
@@ -7,7 +7,7 @@ Description: Adds a Post ID box to Wordpress post editor to display the current 
 
 function add_custom_meta_box()
 {
-    add_meta_box("demo-meta-box", "Post ID", "custom_meta_box_markup", "post", "side", "high", null);
+    add_meta_box("demo-meta-box", "Post information", "custom_meta_box_markup", "post", "side", "high", null);
 }
 add_action("add_meta_boxes", "add_custom_meta_box");
 /*Post ID Meta field*/
@@ -16,8 +16,15 @@ function custom_meta_box_markup($object)
     wp_nonce_field(basename(__FILE__), "meta-box-nonce");
     global $post;
     $id = $post->ID;
+    $featured_img = get_the_post_thumbnail($id);
+    $thumb_url_array = wp_get_attachment_image_src($featured_img, 'thumbnail-size', true);
+	$thumb_url = $thumb_url_array[0];
+	$filesize = filesize( $filename );
+    
         echo '<div>
           	<label for="meta-box-text">Post ID</label>
             <input name="meta-box-text" type="text" value="' . $id . '" style="width: 100%;" readonly />
+            <label for="meta-box-text2">Featured Image Size</label>
+            <input name="meta-box-text2" type="text" value="' . $filesize . '" style="width: 100%;" readonly />
         </div>';
 }
